cc851888da5bce52a1662072f53e784fab57244b
Mashed <mattari.dev@gmail.com>
2024-04-30 15:26:45 -0700

    feat: add base dependencies (#11)

--- dna-frontend/src/app/app.config.ts
+++ dna-frontend/src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideAnimations(),
+    provideRouter(routes),
+    importProvidersFrom(TuiRootModule),
+    provideStore()
+],
 };
