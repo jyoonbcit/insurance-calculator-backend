3a0444b48a5647f7941110d0247d28c73d9b78a8
Mashed <mattari.dev@gmail.com>
2024-05-02 16:28:25 -0700

    feat: add environment variables

---
+++ dna-frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+export const environment = {
+  production: false,
+  supabaseUrl: `${process.env['SUPABASE_URL']}`,
+  supabaseKey: `${process.env['SUPABASE_KEY']}`,
+};
