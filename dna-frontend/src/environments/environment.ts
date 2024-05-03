fd20568cca6206e7b7029111ae7fcc74bee0dab0
Mashed <mattari.dev@gmail.com>
2024-05-02 18:07:26 -0700

    feat: add environment variables and supabase service (#17)

--- dna-frontend/src/environments/environment.ts
+++ dna-frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
-require('dotenv').config();
-
 export const environment = {
   production: false,
-  supabaseUrl: `${process.env['SUPABASE_URL']}`,
-  supabaseKey: `${process.env['SUPABASE_KEY']}`,
+  supabaseUrl: 'https://kkvnnkfuryzvupbgbxjc.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtrdm5ua2Z1cnl6dnVwYmdieGpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ2OTEyMzcsImV4cCI6MjAzMDI2NzIzN30.4SWP5kuHngJqRsrTISXk9xzpzzM2da8pjX7xOm_qBkw',
 };
