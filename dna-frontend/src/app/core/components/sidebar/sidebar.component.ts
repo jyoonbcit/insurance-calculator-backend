44f8f3d70d4cd733ac94b7c3562e8a438039813a
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-06 12:53:15 -0700

    Add themes (#22)

--- dna-frontend/src/app/core/components/sidebar/sidebar.component.ts
+++ dna-frontend/src/app/core/components/sidebar/sidebar.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { TuiSidebarModule } from '@taiga-ui/addon-mobile';
+import { TuiActiveZoneModule } from '@taiga-ui/cdk';
+import { TuiLinkModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [TuiSidebarModule, TuiActiveZoneModule, TuiLinkModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  open = false;
+
+  toggle(open: boolean) {
+    this.open = open;
+  }
+}
--- dna-frontend/src/app/pages/calculator/calculator.component.ts
+++ dna-frontend/src/app/pages/calculator/calculator.component.ts
@@ -3,11 +3,12 @@ import { Component, Inject, NgZone, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { TuiDialogService } from '@taiga-ui/core';
 import { SupabaseService } from 'app/core/services/supabase.service';
+import { SidebarComponent } from 'app/core/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, SidebarComponent],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss',
 })
