c0c0ef27c9f22ed32371433db40897f9d17cd120
Mashed <mattari.dev@gmail.com>
2024-05-07 02:24:47 -0700

    feat: add landing card example to test page (#23)

--- dna-frontend/src/app/pages/test-page/test-page.component.ts
+++ dna-frontend/src/app/pages/test-page/test-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { LandingCardComponent } from 'app/core/components/landing-card/landing-card.component';
 import { ResultCardComponent } from 'app/core/components/result-card/result-card.component';
 import { SubscriptionCardComponent } from 'app/core/components/subscription-card/subscription-card.component';
 
 @Component({
   selector: 'app-test-page',
   standalone: true,
-  imports: [SubscriptionCardComponent, ResultCardComponent],
+  imports: [
+    SubscriptionCardComponent,
+    LandingCardComponent,
+    ResultCardComponent,
+  ],
   templateUrl: './test-page.component.html',
   styleUrl: './test-page.component.scss',
 })
