08d6334a4c9aeee9d0a9d80801a815cdb00e702b
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-09 15:13:48 -0700

    Start working on client page (#30)

--- dna-frontend/src/app/pages/client/client.component.ts
+++ dna-frontend/src/app/pages/client/client.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { TuiInputModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, TuiInputModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClientComponent {}
+export class ClientComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  clientForm = this.formBuilder.group({
+    name: '',
+  });
+
+  async onSubmit(): Promise<void> {
+    try {
+      const name = this.clientForm.value.name as string;
+
+      console.log(name);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error);
+      }
+    } finally {
+      this.clientForm.reset();
+    }
+  }
+}
