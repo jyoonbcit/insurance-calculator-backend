b232efe3e713dfd408f4e420fb4bb2cc5c2411fa
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-08 19:31:19 -0700

    Add horizontal dividers (#22)

--- dna-frontend/src/app/core/components/sidebar/sidebar.component.ts
+++ dna-frontend/src/app/core/components/sidebar/sidebar.component.ts
@@ -3,11 +3,18 @@ import { RouterModule } from '@angular/router';
 import { TuiSidebarModule } from '@taiga-ui/addon-mobile';
 import { TuiActiveZoneModule } from '@taiga-ui/cdk';
 import { TuiLinkModule } from '@taiga-ui/core';
+import { HorizontalDividerComponent } from '../horizontal-divider/horizontal-divider.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [TuiSidebarModule, TuiActiveZoneModule, TuiLinkModule, RouterModule],
+  imports: [
+    TuiSidebarModule,
+    TuiActiveZoneModule,
+    TuiLinkModule,
+    RouterModule,
+    HorizontalDividerComponent,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
