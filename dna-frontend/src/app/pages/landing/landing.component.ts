f3ea0eeb4859e22d4b45f7866ad04c9dea0f0273
nng32 <nng32@my.bcit.ca>
2024-05-10 14:47:40 -0700

    Add logout button to landing page (#33)

--- dna-frontend/src/app/pages/landing/landing.component.ts
+++ dna-frontend/src/app/pages/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TuiButtonModule } from '@taiga-ui/core';
 import { LandingCardComponent } from 'app/core/components/landing-card/landing-card.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [LandingCardComponent],
+  imports: [LandingCardComponent, TuiButtonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
