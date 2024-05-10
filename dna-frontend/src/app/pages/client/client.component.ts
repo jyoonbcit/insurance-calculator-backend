944351b194ad294dfe117766d0540c95b03e13c8
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-10 11:15:14 -0700

    Start adding form elements (#30)

--- dna-frontend/src/app/pages/client/client.component.ts
+++ dna-frontend/src/app/pages/client/client.component.ts
@@ -1,25 +1,56 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { TuiInputModule } from '@taiga-ui/kit';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { TuiDataListModule } from '@taiga-ui/core';
+import {
+  TuiInputDateModule,
+  TuiInputModule,
+  TuiInputNumberModule,
+  tuiInputNumberOptionsProvider,
+} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [ReactiveFormsModule, TuiInputModule],
+  imports: [
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiInputDateModule,
+    TuiInputNumberModule,
+    TuiDataListModule,
+  ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    tuiInputNumberOptionsProvider({
+      decimal: 'never',
+      step: 1,
+    }),
+  ],
 })
 export class ClientComponent {
-  constructor(private formBuilder: FormBuilder) {}
+  form: FormGroup;
 
-  clientForm = this.formBuilder.group({
-    name: '',
-  });
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: new FormControl(),
+      birthDate: new FormControl(),
+      expectedRetirementAge: new FormControl(),
+      province: new FormControl(),
+      annualIncome: new FormControl(),
+      incomeReplacementMultiplier: new FormControl(),
+      taxBracket: new FormControl(),
+    });
+  }
 
   async onSubmit(): Promise<void> {
     try {
-      const name = this.clientForm.value.name as string;
+      const name = this.form.value.name as string;
 
       console.log(name);
     } catch (error) {
@@ -27,7 +58,7 @@ export class ClientComponent {
         console.log(error);
       }
     } finally {
-      this.clientForm.reset();
+      this.form.reset();
     }
   }
 }
