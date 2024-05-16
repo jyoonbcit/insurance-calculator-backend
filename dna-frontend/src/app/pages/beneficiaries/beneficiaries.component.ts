7609e2fcc486068d2346dd2c007b5b57f0113e22
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-16 10:21:13 -0700

    Add appbar and nav tabs to beneficiary page (#58)

--- dna-frontend/src/app/pages/beneficiaries/beneficiaries.component.ts
+++ dna-frontend/src/app/pages/beneficiaries/beneficiaries.component.ts
@@ -1,48 +1,15 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CalculatorComponent } from '../calculator/calculator.component';
+import { TuiTabsModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-beneficiaries',
   standalone: true,
-  imports: [],
+  imports: [CalculatorComponent, TuiTabsModule],
   templateUrl: './beneficiaries.component.html',
   styleUrl: './beneficiaries.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BeneficiariesComponent {
-  form = this.formbuilder.group({
-    beneficiaries: this.formbuilder.array([]),
-  });
-
-  constructor(private formbuilder: FormBuilder) {}
-
-  get beneficiaries() {
-    return this.form.controls['beneficiaries'] as FormArray;
-  }
-
-  addBeneficiary() {
-    const beneficiaryForm = this.formbuilder.group({
-      name: new FormControl(), // String
-      allocation: new FormControl(), // Number
-    });
-    this.beneficiaries.push(beneficiaryForm);
-  }
-
-  deleteBeneficiary(index: number) {
-    this.beneficiaries.removeAt(index);
-  }
-
-  editBeneficiary(
-    index: number,
-    updatedValues: { name?: string; allocation?: number }
-  ) {
-    const currentBeneficiary = this.beneficiaries.at(index) as FormGroup;
-    if (updatedValues.name !== undefined) {
-      currentBeneficiary.controls['name'].setValue(updatedValues.name);
-    }
-    if (updatedValues.allocation !== undefined) {
-      currentBeneficiary.controls['allocation'].setValue(
-        updatedValues.allocation
-      );
-    }
-  }
+  activeItemIndex = 0;
 }
