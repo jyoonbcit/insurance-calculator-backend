afa9c0eb1ee0ee6b59115ae08dc513b780ef9734
nng32 <nng32@my.bcit.ca>
2024-05-16 14:47:15 -0700

    Copy files from total goals page (#71)

--- dna-frontend/src/app/pages/goals/goals.component.ts
+++ dna-frontend/src/app/pages/goals/goals.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TuiDataListModule, TuiNotificationModule } from '@taiga-ui/core';
+import {
+  TuiDataListWrapperModule,
+  TuiInputDateModule,
+  TuiInputModule,
+  TuiInputNumberModule,
+  TuiInputSliderModule,
+  TuiSelectModule,
+  TuiTabsModule,
+} from '@taiga-ui/kit';
+import { CalculatorComponent } from '../calculator/calculator.component';
+import { MultiValueCardComponent } from 'app/core/components/multi-value-card/multi-value-card.component';
 
 @Component({
   selector: 'app-goals',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiInputDateModule,
+    TuiInputNumberModule,
+    TuiInputSliderModule,
+    TuiSelectModule,
+    TuiDataListModule,
+    TuiDataListWrapperModule,
+    TuiTabsModule,
+    TuiNotificationModule,
+    NgIf,
+    MultiValueCardComponent,
+    CalculatorComponent,
+  ],
   templateUrl: './goals.component.html',
   styleUrl: './goals.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GoalsComponent {}
+export class GoalsComponent {
+  activeItemIndex = 0;
+}
