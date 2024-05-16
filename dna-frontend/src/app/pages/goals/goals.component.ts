d4e89338cf9f6e522905dde9ac6d233c45724e04
nng32 <nng32@my.bcit.ca>
2024-05-16 15:14:00 -0700

    Setup data for goals results page (#71)

--- dna-frontend/src/app/pages/goals/goals.component.ts
+++ dna-frontend/src/app/pages/goals/goals.component.ts
@@ -17,8 +17,8 @@ import {
   TuiTabsModule,
 } from '@taiga-ui/kit';
 import { CalculatorComponent } from '../calculator/calculator.component';
-import { MultiValueCardComponent } from 'app/core/components/multi-value-card/multi-value-card.component';
 import { HorizontalDividerComponent } from 'app/core/components/horizontal-divider/horizontal-divider.component';
+import { ValueCardComponent } from 'app/core/components/value-card/value-card.component';
 
 @Component({
   selector: 'app-goals',
@@ -38,7 +38,7 @@ import { HorizontalDividerComponent } from 'app/core/components/horizontal-divid
     TuiButtonModule,
     HorizontalDividerComponent,
     NgIf,
-    MultiValueCardComponent,
+    ValueCardComponent,
     CalculatorComponent,
   ],
   templateUrl: './goals.component.html',
@@ -47,4 +47,49 @@ import { HorizontalDividerComponent } from 'app/core/components/horizontal-divid
 })
 export class GoalsComponent {
   activeItemIndex = 0;
+
+  currentValueOfFixedAssets = {
+    label: 'Total Current Value of Fixed Assets',
+    value: '$1,000,000',
+  };
+  futureValueOfFixedAssets = {
+    label: 'Total Future Value of Fixed Assets',
+    value: '$1,000,000',
+  };
+  currentValueOfLiquidAssets = {
+    label: 'Total Current Value of Liquid Assets',
+    value: '$1,000,000',
+  };
+  futureValueOfLiquidAssets = {
+    label: 'Total Current Value of Liquid Assets',
+    value: '$1,000,000',
+  };
+  currentValueOfAssetsToBeSold = {
+    label: 'Total Current Value of Assets to be Sold',
+    value: '$1,000,000',
+  };
+  futureValueOfAssetsToBeSold = {
+    label: 'Total Future Value of Assets to be Sold',
+    value: '$1,000,000',
+  };
+  percentageLiquidityAllocatedToGoals = {
+    label: '% Liquidity Allocated Towards Goals',
+    value: '50%',
+  };
+  liquidityPreserved = {
+    label: 'Liquidity Preserved',
+    value: '$1,000,000',
+  };
+  liquidityAllocatedTowardsGoals = {
+    label: 'Liquidity Allocated Towards Goals',
+    value: '$1,000,000',
+  };
+  totalSumOfAllGoals = {
+    label: 'Total Sum of All Goals',
+    value: '$1,000,000',
+  };
+  surplus = {
+    label: 'Surplus / Shortfall',
+    value: '$1,000,000',
+  };
 }
