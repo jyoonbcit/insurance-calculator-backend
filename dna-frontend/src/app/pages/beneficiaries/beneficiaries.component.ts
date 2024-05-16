75b3d48fe687da653496772f12e936194a42064c
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-16 12:59:44 -0700

    Finish layout (#58)

--- dna-frontend/src/app/pages/beneficiaries/beneficiaries.component.ts
+++ dna-frontend/src/app/pages/beneficiaries/beneficiaries.component.ts
@@ -8,6 +8,8 @@ import {
 import { TuiButtonModule } from '@taiga-ui/core';
 import { NgIf } from '@angular/common';
 import { HorizontalDividerComponent } from 'app/core/components/horizontal-divider/horizontal-divider.component';
+import { PieChartComponent } from 'app/core/components/pie-chart/pie-chart.component';
+import { BarChartComponent } from 'app/core/components/bar-chart/bar-chart.component';
 
 @Component({
   selector: 'app-beneficiaries',
@@ -20,6 +22,8 @@ import { HorizontalDividerComponent } from 'app/core/components/horizontal-divid
     TuiButtonModule,
     NgIf,
     HorizontalDividerComponent,
+    PieChartComponent,
+    BarChartComponent,
   ],
   templateUrl: './beneficiaries.component.html',
   styleUrl: './beneficiaries.component.scss',
