17515014a112eb87297f9c92647972554fe7e695
nng32 <nng32@my.bcit.ca>
2024-05-16 11:35:49 -0700

    Copy over client pages to total needs (#64)

--- dna-frontend/src/app/pages/total-needs/total-needs.component.ts
+++ dna-frontend/src/app/pages/total-needs/total-needs.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TuiDataListModule } from '@taiga-ui/core';
+import {
+  TuiDataListWrapperModule,
+  TuiInputDateModule,
+  TuiInputModule,
+  TuiInputNumberModule,
+  TuiSelectModule,
+  TuiTabsModule,
+} from '@taiga-ui/kit';
+import { ValueCardComponent } from 'app/core/components/value-card/value-card.component';
+import { CalculatorComponent } from '../calculator/calculator.component';
 
 @Component({
-  selector: 'app-total-needs',
+  selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiInputDateModule,
+    TuiInputNumberModule,
+    TuiSelectModule,
+    TuiDataListModule,
+    TuiDataListWrapperModule,
+    TuiTabsModule,
+    NgIf,
+    ValueCardComponent,
+    CalculatorComponent,
+  ],
   templateUrl: './total-needs.component.html',
   styleUrl: './total-needs.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TotalNeedsComponent {}
+export class TotalNeedsComponent {
+  activeItemIndex = 0;
+}
