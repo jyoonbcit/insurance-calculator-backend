d4e23b91387ca70000b53d027c32d2728e0dce79
nng32 <nng32@my.bcit.ca>
2024-05-16 13:52:25 -0700

    Replace dashes with spaces in page name (#64)

--- dna-frontend/src/app/pages/calculator/calculator.component.ts
+++ dna-frontend/src/app/pages/calculator/calculator.component.ts
@@ -33,7 +33,7 @@ export class CalculatorComponent {
 
   toTitleCase(str: string): string {
     return str
-      .split(' ')
+      .split('-')
       .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(' ');
   }
