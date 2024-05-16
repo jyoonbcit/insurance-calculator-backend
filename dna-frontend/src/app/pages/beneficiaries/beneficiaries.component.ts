ca9fc2f42ed910930c551e2cec4ee93fb135cf6e
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-16 11:53:16 -0700

    Add all input components (#58)

--- dna-frontend/src/app/pages/beneficiaries/beneficiaries.component.ts
+++ dna-frontend/src/app/pages/beneficiaries/beneficiaries.component.ts
@@ -1,11 +1,26 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CalculatorComponent } from '../calculator/calculator.component';
-import { TuiTabsModule } from '@taiga-ui/kit';
+import {
+  TuiInputModule,
+  TuiInputNumberModule,
+  TuiTabsModule,
+} from '@taiga-ui/kit';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { NgIf } from '@angular/common';
+import { HorizontalDividerComponent } from 'app/core/components/horizontal-divider/horizontal-divider.component';
 
 @Component({
   selector: 'app-beneficiaries',
   standalone: true,
-  imports: [CalculatorComponent, TuiTabsModule],
+  imports: [
+    CalculatorComponent,
+    TuiTabsModule,
+    TuiInputModule,
+    TuiInputNumberModule,
+    TuiButtonModule,
+    NgIf,
+    HorizontalDividerComponent,
+  ],
   templateUrl: './beneficiaries.component.html',
   styleUrl: './beneficiaries.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
