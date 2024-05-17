39e220a587c3c0c879da08e3ac9d14146381fa27
nng32 <nng32@my.bcit.ca>
2024-05-17 14:30:26 -0700

    Add notification to action bar (#73)

--- dna-frontend/src/app/core/components/action-bar/action-bar.component.ts
+++ dna-frontend/src/app/core/components/action-bar/action-bar.component.ts
@@ -1,16 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiNotificationModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-action-bar',
   standalone: true,
-  imports: [TuiButtonModule],
+  imports: [TuiButtonModule, TuiNotificationModule, NgIf],
   templateUrl: './action-bar.component.html',
   styleUrl: './action-bar.component.scss',
 })
 export class ActionBarComponent {
   @Input() leftButtonLabel = 'Cancel';
   @Input() rightButtonLabel = 'Save';
+
+  @Input() isNotificationShown = false;
+  @Input() notificationLabel = '';
 
   @Output() leftButtonClick = new EventEmitter();
   @Output() rightButtonClick = new EventEmitter();
