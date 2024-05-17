c953930f714c78b2da5d02ca24e81a97a4e82cde
nng32 <nng32@my.bcit.ca>
2024-05-17 14:10:07 -0700

    Change some input fields to number (#73)

--- dna-frontend/src/app/pages/debt-edit/debt-edit.component.ts
+++ dna-frontend/src/app/pages/debt-edit/debt-edit.component.ts
@@ -1,25 +1,9 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  TuiDataListModule,
-  TuiNotificationModule,
-  TuiButtonModule,
-} from '@taiga-ui/core';
-import {
-  TuiInputModule,
-  TuiInputDateModule,
-  TuiInputNumberModule,
-  TuiInputSliderModule,
-  TuiSelectModule,
-  TuiDataListWrapperModule,
-  TuiTabsModule,
-  TuiCheckboxBlockModule,
-} from '@taiga-ui/kit';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiInputModule, TuiInputNumberModule } from '@taiga-ui/kit';
 import { HorizontalDividerComponent } from 'app/core/components/horizontal-divider/horizontal-divider.component';
-import { LineChartComponent } from 'app/core/components/line-chart/line-chart.component';
-import { ValueCardComponent } from 'app/core/components/value-card/value-card.component';
-import { CalculatorComponent } from '../calculator/calculator.component';
 import { HeaderBarComponent } from 'app/core/components/header-bar/header-bar.component';
 import { ActionBarComponent } from 'app/core/components/action-bar/action-bar.component';
 import { BottomBarComponent } from 'app/core/components/bottom-bar/bottom-bar.component';
@@ -30,21 +14,10 @@ import { BottomBarComponent } from 'app/core/components/bottom-bar/bottom-bar.co
   imports: [
     ReactiveFormsModule,
     TuiInputModule,
-    TuiInputDateModule,
     TuiInputNumberModule,
-    TuiInputSliderModule,
-    TuiSelectModule,
-    TuiDataListModule,
-    TuiDataListWrapperModule,
-    TuiTabsModule,
-    TuiNotificationModule,
-    TuiCheckboxBlockModule,
     TuiButtonModule,
     HorizontalDividerComponent,
     NgIf,
-    ValueCardComponent,
-    CalculatorComponent,
-    LineChartComponent,
     HeaderBarComponent,
     ActionBarComponent,
     BottomBarComponent,
