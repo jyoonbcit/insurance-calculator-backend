fcd1715b13b470dda57320e7d7f109a0c7d41188
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-17 12:53:36 -0700

    Create layout and styling for results (#76)

--- dna-frontend/src/app/pages/businesses/businesses.component.ts
+++ dna-frontend/src/app/pages/businesses/businesses.component.ts
@@ -8,6 +8,9 @@ import {
 } from '@taiga-ui/kit';
 import { HorizontalDividerComponent } from 'app/core/components/horizontal-divider/horizontal-divider.component';
 import { TuiButtonModule } from '@taiga-ui/core';
+import { LineChartComponent } from 'app/core/components/line-chart/line-chart.component';
+import { ValueCardComponent } from 'app/core/components/value-card/value-card.component';
+import { ValueListCardComponent } from 'app/core/components/value-list-card/value-list-card.component';
 
 @Component({
   selector: 'app-businesses',
@@ -20,10 +23,19 @@ import { TuiButtonModule } from '@taiga-ui/core';
     HorizontalDividerComponent,
     TuiTabsModule,
     TuiButtonModule,
+    LineChartComponent,
+    ValueCardComponent,
+    ValueListCardComponent,
   ],
   templateUrl: './businesses.component.html',
   styleUrl: './businesses.component.scss',
 })
 export class BusinessesComponent {
   activeItemIndex = 0;
+
+  valueList = [
+    { label: 'EBITDA Contribution ($CAD)', value: '$600,000.00' },
+    { label: 'Share Value ($CAD)', value: '$1,000,000' },
+    { label: 'Liquidation Disparity ($CAD)', value: '$1,000,000' },
+  ];
 }
