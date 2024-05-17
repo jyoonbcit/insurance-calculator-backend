dfc164661b644d29255e024be28a3888642849a6
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-17 12:16:31 -0700

    Add delete button in beneficiaries page

--- dna-frontend/src/app/pages/businesses/businesses.component.ts
+++ dna-frontend/src/app/pages/businesses/businesses.component.ts
@@ -1,52 +1,29 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { CalculatorComponent } from '../calculator/calculator.component';
+import { NgIf } from '@angular/common';
+import {
+  TuiInputModule,
+  TuiInputNumberModule,
+  TuiTabsModule,
+} from '@taiga-ui/kit';
+import { HorizontalDividerComponent } from 'app/core/components/horizontal-divider/horizontal-divider.component';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-businesses',
   standalone: true,
-  imports: [],
+  imports: [
+    CalculatorComponent,
+    NgIf,
+    TuiInputModule,
+    TuiInputNumberModule,
+    HorizontalDividerComponent,
+    TuiTabsModule,
+    TuiButtonModule,
+  ],
   templateUrl: './businesses.component.html',
   styleUrl: './businesses.component.scss',
 })
 export class BusinessesComponent {
-  form = this.formBuilder.group({
-    businesses: this.formBuilder.array([]),
-  });
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  get businesses() {
-    return this.form.controls['businesses'] as FormArray;
-  }
-
-  addBusiness() {
-    const businessForm = this.formBuilder.group({
-      name: new FormControl(), // String
-      valuation: new FormControl(), // Number
-    });
-    this.businesses.push(businessForm);
-  }
-
-  deleteBusiness(index: number) {
-    this.businesses.removeAt(index);
-  }
-
-  editBusiness(
-    index: number,
-    updatedValues: {
-      name?: string;
-      valuation?: number;
-      ebitda?: number;
-      apperciationRate?: number;
-      term?: number;
-    }
-  ) {
-    const currentBusiness = this.businesses.at(index) as FormGroup;
-    if (updatedValues.name !== undefined) {
-      currentBusiness.controls['name'].setValue(updatedValues.name);
-    }
-    if (updatedValues.valuation !== undefined) {
-      currentBusiness.controls['valuation'].setValue(updatedValues.valuation);
-    }
-  }
+  activeItemIndex = 0;
 }
