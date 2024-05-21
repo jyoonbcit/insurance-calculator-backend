e916c3498d3b0897e4c882780aa70c3f02f30246
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-21 14:48:03 -0700

    Reverse the query Input for business route as per @m-a-s-h-e-d request (#76)

--- dna-frontend/src/app/app.routes.ts
+++ dna-frontend/src/app/app.routes.ts
@@ -78,7 +78,7 @@ export const routes: Routes = [
     title: 'DNA | Businesses',
   },
   {
-    path: 'business/:businessId/:clientId',
+    path: 'business/:clientId/:businessId',
     loadComponent: () =>
       import('./pages/business-edit/business-edit.component').then(
         com => com.BusinessEditComponent
