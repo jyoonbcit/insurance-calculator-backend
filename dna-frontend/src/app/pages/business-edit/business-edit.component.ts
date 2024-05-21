03f7fc5c0c897c95ff61b3548f97415d99b60f91
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2024-05-21 13:10:53 -0700

    Add FormGroup and FormArray implementation (#76)

--- dna-frontend/src/app/pages/business-edit/business-edit.component.ts
+++ dna-frontend/src/app/pages/business-edit/business-edit.component.ts
@@ -1,7 +1,12 @@
 import { Location, NgIf } from '@angular/common';
-import { Component, NgZone } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, Input, NgZone } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { TuiButtonModule, TuiNotificationModule } from '@taiga-ui/core';
 import {
   TuiInputModule,
@@ -34,12 +39,48 @@ import { HorizontalDividerComponent } from 'app/core/components/horizontal-divid
 })
 export class BusinessEditComponent {
   activeItemIndex = 0;
+  @Input() businessId: number = 0;
+  readonly businessEditInformationForm = new FormGroup({
+    name: new FormControl(),
+    valuation: new FormControl(),
+    ebitda: new FormControl(),
+    appreciationRate: new FormControl(),
+    term: new FormControl(),
+  });
+  readonly businessEditShareholdersForm = new FormGroup({
+    shareholders: new FormArray([]),
+  });
 
   constructor(
-    private router: Router,
     private zone: NgZone,
-    private location: Location
-  ) {}
+    private location: Location,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe(params => {
+      this.businessId = +params['id'];
+    });
+  }
+
+  get shareholders(): FormArray {
+    return this.businessEditShareholdersForm.get('shareholders') as FormArray;
+  }
+
+  createShareholder(): FormGroup {
+    return new FormGroup({
+      name: new FormControl(),
+      sharePercentage: new FormControl(),
+      insuranceCoverage: new FormControl(),
+      ebitdaContributionPercentage: new FormControl(),
+    });
+  }
+
+  addShareholder() {
+    this.shareholders.push(this.createShareholder());
+  }
+
+  removeShareholder(index: number) {
+    this.shareholders.removeAt(index);
+  }
 
   cancel() {
     this.zone.run(() => {
